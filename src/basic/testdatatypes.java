8c40e8f8b4b42682b8fc30e8bb8bdda3af12b8e6
Harsha Ganesh <harshaganesh10@gmail.com>
2024-12-20 01:53:55 +0530

    Initial commit

---
+++ src/basic/testdatatypes.java
@@ -0,0 +1,27 @@
+package basic;
+
+public class testdatatypes {
+    public static void main(String[] args){
+        int i = 899;
+        int j = 498;
+        System.out.println(i+j);
+        System.out.println(i-j);
+        System.out.println(i*j);
+        System.out.println(i/j);
+        System.out.println(i%j);
+        System.out.println(++i); //pre
+        System.out.println(i++); //post
+        System.out.println(i);
+        System.out.println(--j);
+        System.out.println(j--);
+        System.out.println(j);
+        System.out.println(i==j);
+        System.out.println(i!=j);
+        System.out.println(i>j);
+        System.out.println(i<j);
+        System.out.println(i>=j);
+        System.out.println(i<=j);
+        System.out.println(i < j && i==j);
+
+    }
+}
